fe0f9c199ffc6d8b29daa53dbae38d0f00a91ad9
Cix8 <francesco.natal_fn@libero.it>
2022-05-23 14:55:42 +0200

    add milestone 2.1 (languageFlag)

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -7,7 +7,21 @@
       <h2>Titolo Originale: {{ thisElement.original_title }}</h2>
     </div>
     <div class="lang">
-      <p>Lingua: {{ thisElement.original_language.toUpperCase() }}</p>
+      <template v-if="langFlag(thisElement.original_language).length > 2">
+        <div class="flag-container">
+          <img
+            :src="
+              require('../assets/img/' +
+                langFlag(thisElement.original_language) +
+                '-flag.png')
+            "
+            alt=""
+          />
+        </div>
+      </template>
+      <template v-else>
+        <span>Lingua: {{ thisElement.original_language.toUpperCase() }}</span>
+      </template>
     </div>
     <div class="rating">
       <strong>Voto: {{ thisElement.popularity }}</strong>
@@ -21,11 +35,39 @@ export default {
   props: {
     thisElement: Object,
   },
+  methods: {
+    langFlag(lang) {
+      let result;
+      let checkLang = lang.toLowerCase();
+      if (checkLang === "it") {
+        result = "italian";
+      } else if (checkLang === "en") {
+        result = "english";
+      } else if (checkLang === "pt") {
+        result = "portugues";
+      } else if (checkLang === "es") {
+        result = "espanol";
+      } else {
+        result = lang.toUpperCase();
+      }
+      return result;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 li {
   margin: 2rem 0;
+
+  .lang {
+    .flag-container {
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      overflow: hidden;
+      margin: 1rem 0;
+    }
+  }
 }
 </style>
