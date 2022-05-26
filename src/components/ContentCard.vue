9ca67219a6a75514f470190d2c11de126f732bcb
Cix8 <francesco.natal_fn@libero.it>
2022-05-26 13:07:55 +0200

    added 'a touch' of 3D, useless features deleted

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -1,90 +1,95 @@
 <template>
-  <li
-    @mouseover="showInfo = true"
-    @mouseleave="(showInfo = false), (showDetails = false)"
-  >
-    <template v-if="thisContent.poster_path !== null">
-      <div class="img-container">
-        <img
-          :src="'https://image.tmdb.org/t/p/original' + thisContent.poster_path"
-          :alt="thisContent.title ? thisContent.title : thisContent.name"
-        />
-      </div>
-    </template>
-    <template v-else>
-      <div class="img-placeholder">
-        <h3>{{ thisContent.title ? thisContent.title : thisContent.name }}</h3>
-      </div>
-    </template>
-    <div class="info" v-if="showInfo" @click="findCastAndGenres(thisContent)">
-      <div class="details" v-if="showDetails">
-        <div class="cast">
-          <div class="cast-title">Cast:</div>
-          <div v-for="(people, index) in cast" :key="index">
-            {{ people.original_name }}
+  <li @mouseleave="showDetails = false">
+    <div class="card-contents">
+      <template v-if="thisContent.poster_path !== null">
+        <div class="img-container">
+          <img
+            :src="
+              'https://image.tmdb.org/t/p/original' + thisContent.poster_path
+            "
+            :alt="thisContent.title ? thisContent.title : thisContent.name"
+          />
+        </div>
+      </template>
+      <template v-else>
+        <div class="img-placeholder">
+          <h3>
+            {{ thisContent.title ? thisContent.title : thisContent.name }}
+          </h3>
+        </div>
+      </template>
+      <div class="info" @click="findCastAndGenres(thisContent)">
+        <div class="details" v-if="showDetails">
+          <div class="cast">
+            <div class="cast-title">Cast:</div>
+            <div v-for="(people, index) in cast" :key="index">
+              {{ people.original_name }}
+            </div>
+          </div>
+          <div class="genre">
+            <div class="genre-title">Genere:</div>
+            <div v-for="(genre, index) in genres" :key="index">
+              {{ genre.name }}
+            </div>
           </div>
         </div>
-        <div class="genre">
-          <div class="genre-title">Genere:</div>
-          <div v-for="(genre, index) in genres" :key="index">
-            {{ genre.name }}
-          </div>
+        <div class="title">
+          <h3>
+            Titolo:
+            <span>{{
+              thisContent.title ? thisContent.title : thisContent.name
+            }}</span>
+          </h3>
         </div>
-      </div>
-      <div class="title">
-        <h3>
-          Titolo:
-          <span>{{
-            thisContent.title ? thisContent.title : thisContent.name
-          }}</span>
-        </h3>
-      </div>
-      <div class="original-title">
-        <h3>
-          Titolo Originale:
-          <span>{{
-            thisContent.original_title
-              ? thisContent.original_title
-              : thisContent.original_name
-          }}</span>
-        </h3>
-      </div>
-      <div class="lang">
-        <template v-if="availableFlags.includes(thisContent.original_language)">
-          <div class="flag-container">
-            <img
-              :src="
-                require('../assets/img/' +
-                  thisContent.original_language +
-                  '-flag.png')
-              "
-              :alt="thisContent.original_language"
-            />
-          </div>
-        </template>
-        <template v-else>
-          <div class="text">
-            <span
-              >Lingua: {{ thisContent.original_language.toUpperCase() }}</span
-            >
-          </div>
-        </template>
-      </div>
-      <div class="rating" v-if="thisContent.vote_average">
-        <strong
-          >Voto:
-          <i
-            v-for="(star, index) in starRating(thisContent.vote_average)"
-            :key="index"
-            class="fas fa-star"
-            :class="{ 'full-star': star }"
-          ></i>
-        </strong>
-      </div>
-      <div class="overview" v-if="thisContent.overview">
-        <p class="bold">
-          Overview: <small>{{ thisContent.overview }}</small>
-        </p>
+        <div class="original-title">
+          <h3>
+            Titolo Originale:
+            <span>{{
+              thisContent.original_title
+                ? thisContent.original_title
+                : thisContent.original_name
+            }}</span>
+          </h3>
+        </div>
+        <div class="lang">
+          <template
+            v-if="availableFlags.includes(thisContent.original_language)"
+          >
+            <div class="flag-container">
+              <img
+                :src="
+                  require('../assets/img/' +
+                    thisContent.original_language +
+                    '-flag.png')
+                "
+                :alt="thisContent.original_language"
+              />
+            </div>
+          </template>
+          <template v-else>
+            <div class="text">
+              <span
+                >Lingua: {{ thisContent.original_language.toUpperCase() }}</span
+              >
+            </div>
+          </template>
+        </div>
+        <div class="rating" v-if="thisContent.vote_average">
+          <strong
+            >Voto:
+            <i
+              v-for="(star, index) in starRating(thisContent.vote_average)"
+              :key="index"
+              class="fas fa-star"
+              :class="{ 'full-star': star }"
+            ></i>
+          </strong>
+        </div>
+        <div class="overview" v-if="thisContent.overview">
+          <p class="bold">
+            Overview: <small>{{ thisContent.overview }}</small>
+          </p>
+        </div>
       </div>
     </div>
   </li>
@@ -104,7 +109,6 @@ export default {
       genres: [],
       availableFlags: ["de", "en", "es", "fr", "hi", "it", "ja", "no", "pt"],
       showDetails: false,
-      showInfo: false,
     };
   },
   methods: {
